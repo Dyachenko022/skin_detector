0f362b1e8099f68c7649bc69a9c56ef630cc2aeb
Dmitry Dyachenko <spareemail41@gmail.com>
2023-02-07 00:32:37 +0300

    delete useless formdata

--- Functions/Main.ts
+++ Functions/Main.ts
@@ -3,9 +3,7 @@ import * as FileSystem from 'expo-file-system';
 
 // Отправка фотографии на сервер
 export const sendUserPhoto = async ( imageUrl: string ) => {
-  const formData = new FormData();
   const file = await FileSystem.readAsStringAsync(imageUrl, { encoding: FileSystem.EncodingType.Base64 });
-  formData.append("data[]", `data:image/jpeg;base64,${file}`);
 
   const requestUrl = 'https://cybernatedart-skin-disease-detection.hf.space/api/push'
   const request = await axios.post(requestUrl, {
